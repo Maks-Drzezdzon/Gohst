68a36d72a8d26d690fcb0722eea79f49c4a0a8ad
maksthorn <maksdrzezdzon@gmail.com>
2020-09-13 12:53:57 +0100

    setting up GoLand for project

--- main.go
+++ main.go
@@ -1,2 +1,3 @@
-// ToDo add lexer to also attach the line number, column number and filename to a token
-// "error: expected semicolon token. line 42, column 23,"  
+package Gohst
+// ToDo add lexer to also attach the line number, column number and filename to a
+// token "error: expected semicolon token. line 42, column 23,"
